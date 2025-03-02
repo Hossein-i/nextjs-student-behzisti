cada6cdbe252bcd19c6a6de0278dc517955f4047
Hossein-i <20hossein00@gmail.com>
2025-03-02 16:56:29 +0330

    fix: some problems from template

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended'
+  ),
+];
+
+export default eslintConfig;
