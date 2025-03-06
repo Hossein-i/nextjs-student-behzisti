14683615392bfb32692de40247a56e3623803350
Hossein-i <20hossein00@gmail.com>
2025-03-06 23:11:06 +0330

    refactor(graphql): change import statements to use 'type' for type imports

--- src/shared/api/graphql/documents/mutations/auth.mutation.ts
+++ src/shared/api/graphql/documents/mutations/auth.mutation.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 import { apolloClient } from '../../base';
-import {
+import type {
   PasswordResetRequestMutation,
   PasswordResetRequestMutationVariables,
   SignInMutation,
--- src/shared/api/graphql/documents/queries/education.query.ts
+++ src/shared/api/graphql/documents/queries/education.query.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 import { apolloClient } from '../../base';
-import {
+import type {
   GetEducationLevelsQuery,
   GetEducationLevelsQueryVariables,
   GetTermsByEducationLevelQuery,
