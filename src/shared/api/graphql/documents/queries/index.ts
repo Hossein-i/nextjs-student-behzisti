228218c85d6c422f6bcf1be0569ce74c156cd8cc
Hossein-i <20hossein00@gmail.com>
2025-03-07 01:16:24 +0330

    feat(graphql): add address and university queries and types for improved data fetching

--- src/shared/api/graphql/documents/queries/index.ts
+++ src/shared/api/graphql/documents/queries/index.ts
@@ -1,13 +1,62 @@
+// documents
+export {
+  countiesByProvinceDocument,
+  districtsByCountyDocument,
+  provincesDocument,
+  subDistrictsByDistrictDocument,
+} from './address.query';
+export { inquiryDocument } from './auth.query';
+export {
+  educationLevelsDocument,
+  termsByEducationLevelDocument,
+} from './education.query';
+export {
+  univeristyLevelGroupsDocument,
+  univeristyLevelSubGroupsDocument,
+  univeristyTypesDocument,
+  universityDepartmentsDocument,
+  universityLevelsDocument,
+  universityMajorsDocument,
+} from './univeristy.query';
+
+// queries
+export {
+  countiesByProvinceQuery,
+  districtsByCountyQuery,
+  provincesQuery,
+  subDistrictsByDistrictQuery,
+} from './address.query';
+export { inquiryQuery } from './auth.query';
+export {
+  educationLevelsQuery,
+  termsByEducationLevelQuery,
+} from './education.query';
+export {
+  univeristyLevelGroupsQuery,
+  univeristyLevelSubGroupsQuery,
+  univeristyTypesQuery,
+  universityDepartmentsQuery,
+  universityLevelsQuery,
+  universityMajorsQuery,
+} from './univeristy.query';
+
 // types
+export type {
+  CountiesByProvinceQueryReturn,
+  DistrictsByCountyQueryReturn,
+  ProvincesQueryReturn,
+  SubDistrictsByDistrictQueryReturn,
+} from './address.query';
+export type { InquiryQueryReturn } from './auth.query';
 export type {
   EducationLevelsQueryReturn,
   TermsByEducationLevelQueryReturn,
 } from './education.query';
-
-// queries
-export {
-  educationLevelsDocument,
-  educationLevelsQuery,
-  termsByEducationLevelDocument,
-  termsByEducationLevelQuery,
-} from './education.query';
+export type {
+  UniveristyLevelGroupsQueryReturn,
+  UniveristyLevelSubGroupsQueryReturn,
+  UniveristyTypesQueryReturn,
+  UniversityDepartmentsQueryReturn,
+  UniversityLevelsQueryReturn,
+  UniversityMajorsQueryReturn,
+} from './univeristy.query';
